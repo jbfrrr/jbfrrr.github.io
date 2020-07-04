5c6eeea9425d2b1b68c2a6bf3c805d4082bc309e
jbfrrr <jbfrrr@protonmail.com>
2020-07-04 23:50:43 +1000

    Restart project using Gridsome

---
+++ src/.temp/config.js
@@ -0,0 +1,8 @@
+export default {
+  "trailingSlash": true,
+  "pathPrefix": "",
+  "titleTemplate": "%s - Devclectic",
+  "siteUrl": "",
+  "version": "0.7.18",
+  "catchLinks": true
+}
---
+++ src/.temp/icons.js
@@ -0,0 +1,41 @@
+export default {
+  "touchiconMimeType": "image/png",
+  "faviconMimeType": "image/png",
+  "precomposed": false,
+  "touchicons": [
+    {
+      "width": 76,
+      "src": "/assets/static/src/favicon.png?width=76&key=567e67c"
+    },
+    {
+      "width": 152,
+      "src": "/assets/static/src/favicon.png?width=152&key=567e67c"
+    },
+    {
+      "width": 120,
+      "src": "/assets/static/src/favicon.png?width=120&key=567e67c"
+    },
+    {
+      "width": 167,
+      "src": "/assets/static/src/favicon.png?width=167&key=567e67c"
+    },
+    {
+      "width": 180,
+      "src": "/assets/static/src/favicon.png?width=180&key=567e67c"
+    }
+  ],
+  "favicons": [
+    {
+      "width": 16,
+      "src": "/assets/static/src/favicon.png?width=16&key=868226a"
+    },
+    {
+      "width": 32,
+      "src": "/assets/static/src/favicon.png?width=32&key=868226a"
+    },
+    {
+      "width": 96,
+      "src": "/assets/static/src/favicon.png?width=96&key=868226a"
+    }
+  ]
+}
