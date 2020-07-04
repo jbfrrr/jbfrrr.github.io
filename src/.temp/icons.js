32d8ae26b960bf378993cc48fb53267d10cbcdc4
jbfrrr <jbfrrr@protonmail.com>
2020-07-04 23:50:43 +1000

    Restart project using Gridsome

---
+++ src/.temp/icons.js
@@ -0,0 +1,41 @@
+export default {
+  "touchiconMimeType": "image/png",
+  "faviconMimeType": "image/png",
+  "precomposed": false,
+  "touchicons": [
+    {
+      "width": 76,
+      "src": "/assets/static/favicon.f22e9f3.b7e0c967586518ac9fc5f789db72d75c.png"
+    },
+    {
+      "width": 152,
+      "src": "/assets/static/favicon.62d22cb.b7e0c967586518ac9fc5f789db72d75c.png"
+    },
+    {
+      "width": 120,
+      "src": "/assets/static/favicon.1539b60.b7e0c967586518ac9fc5f789db72d75c.png"
+    },
+    {
+      "width": 167,
+      "src": "/assets/static/favicon.dc0cdc5.b7e0c967586518ac9fc5f789db72d75c.png"
+    },
+    {
+      "width": 180,
+      "src": "/assets/static/favicon.7b22250.b7e0c967586518ac9fc5f789db72d75c.png"
+    }
+  ],
+  "favicons": [
+    {
+      "width": 16,
+      "src": "/assets/static/favicon.ce0531f.b7e0c967586518ac9fc5f789db72d75c.png"
+    },
+    {
+      "width": 32,
+      "src": "/assets/static/favicon.ac8d93a.b7e0c967586518ac9fc5f789db72d75c.png"
+    },
+    {
+      "width": 96,
+      "src": "/assets/static/favicon.b9532cc.b7e0c967586518ac9fc5f789db72d75c.png"
+    }
+  ]
+}
---
+++ src/pages/Index.vue
@@ -0,0 +1,41 @@
+<template>
+  <Layout>
+    <g-image alt="hacker" src="~/hacker.png" width="135" />
+
+    <h1>Hello world!</h1>
+    <p>I'm a software engineer determined to this planet a little bit better each day.</p>
+    <p>This is where I hope to share my thoughts, ideas, opinions and nuggets of wisdom I pick up as I go.</p>
+    <p>I'm exploring the world right now.</p>
+    <p>Hope to see you around!</p>
+    <br/>
+    <br/>
+    <br/>
+    <small>Current in: <strong>Sydney, Australia</strong></small>
+    <br/>
+    <small>Reach me at <strong>jbfrrr@protonmail.com</strong></small>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <small>Icons made by Freepik from www.flaticon.com"</small>
+
+
+
+  </Layout>
+</template>
+
+<script>
+export default {
+  //metaInfo: {
+  //  title: ''
+  //}
+}
+</script>
+
+<style>
+.home-links a {
+  margin-right: 1rem;
+}
+</style>
