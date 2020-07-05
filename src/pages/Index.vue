4cb5a7750856e23ddc4d8d9a7a2a1dc1ec14c769
jbfrrr <jbfrrr@protonmail.com>
2020-07-05 14:39:51 +1000

    Fix some copy

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,14 +3,14 @@
     <g-image alt="hacker" src="~/hacker.png" width="135" />
 
     <h1>Hello world!</h1>
-    <p>I'm a software engineer determined to this planet a little bit better each day.</p>
+    <p>I'm a software engineer determined to make this planet a little bit better each day.</p>
     <p>This is where I hope to share my thoughts, ideas, opinions and nuggets of wisdom I pick up as I go.</p>
     <p>I'm exploring the world right now.</p>
     <p>Hope to see you around!</p>
     <br/>
     <br/>
     <br/>
-    <small>Current in: <strong>Sydney, Australia</strong></small>
+    <small>Currently in: <strong>Sydney, Australia</strong></small>
     <br/>
     <small>Reach me at <strong>jbfrrr@protonmail.com</strong></small>
     <br/>
@@ -19,7 +19,7 @@
     <br/>
     <br/>
     <br/>
-    <small>Icons made by Freepik from www.flaticon.com"</small>
+    <small>Icons made by Freepik from www.flaticon.com</small>
 
 
 
