7f51e4ea1a88e579a1ce97f8c0a18150dcca547b
jbfrrr <jbfrrr@protonmail.com>
2020-07-05 16:06:23 +1000

    Apply more copy changes

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -2,9 +2,9 @@
   <div class="layout">
     <header class="header">
       <nav class="nav">
-        <g-link class="nav__link" to="/">Home</g-link>
+        <!--<g-link class="nav__link" to="/">Home</g-link>
         <g-link class="nav__link" to="/">Blog</g-link>
-        <g-link class="nav__link" to="/">Portfolio</g-link>
+        <g-link class="nav__link" to="/">Portfolio</g-link> -->
       </nav>
     </header>
     <slot/>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,27 +1,41 @@
 <template>
   <Layout>
     <g-image alt="hacker" src="~/hacker.png" width="135" />
-
-    <h1>Hello world!</h1>
+    <br/>
+    <h1>Hello World!</h1>
     <p>I'm a software engineer determined to make this planet a little bit better each day.</p>
     <p>This is where I hope to share my thoughts, ideas, opinions and nuggets of wisdom I pick up as I go.</p>
     <p>I'm exploring the world right now.</p>
     <p>Hope to see you around!</p>
     <br/>
+    <!---
+    <br/>
+    <br/>
+    <h2>Posts</h2>
+    <br/>
+    <br/>
+    <br/>
+    <h2>Projects</h2>
+
+    -->
+    <br/>
+    <br/>
+    <br/>
     <br/>
     <br/>
     <small>Currently in: <strong>Sydney, Australia</strong></small>
     <br/>
-    <small>Reach me at <strong>jbfrrr@protonmail.com</strong></small>
+    <small>Reach me at <strong><a href="mailto:jbfrrr@protonmail.com">jbfrrr@protonmail.com</a></strong></small>
     <br/>
     <br/>
     <br/>
     <br/>
     <br/>
-    <br/>
-    <small>Icons made by Freepik from www.flaticon.com</small>
-
-
+    <footer>
+      <small>Copyright © 2020 jbfrrr</small>
+      <br/>
+      <small>Icons made by Freepik from www.flaticon.com</small>
+    </footer>
 
   </Layout>
 </template>
